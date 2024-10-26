ff479256a35977e17b76f7bade2b695d4a4da14f
suzukimain <131413573+suzukimain@users.noreply.github.com>
2024-10-27 07:40:28 +0900

    Update mix_class.py

--- src/diffusers/pipelines/easy/model_search/search/mix_class.py
+++ src/diffusers/pipelines/easy/model_search/search/mix_class.py
@@ -1,11 +1,9 @@
 from .pipeline_search_for_HuggingFace import HFSearchPipeline
 from .pipeline_search_for_Civitai import CivitaiSearchPipeline
-from .pipeline_search_for_hub import ModelSearchPipeline
 
 class Config_Mix(
     HFSearchPipeline,
-    CivitaiSearchPipeline,
-    ModelSearchPipeline
+    CivitaiSearchPipeline
     ):
     # Resolve multiple inheritance order (MRO) error
     def __init__(self):
